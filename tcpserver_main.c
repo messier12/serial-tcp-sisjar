188dab20ec95e8d303880a1c6b4869250e63ad11
Dion <gumball3point@gmail.com>
2021-12-12 00:39:23 +0700

    server thread, need to implement signals

--- tcpserver_main.c
+++ tcpserver_main.c
@@ -5,53 +5,89 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <pthread.h>
+#include <unistd.h>
+
+
 #define MAX 80
 #define SA struct sockaddr
-   
-// Function designed for chat between client and server.
-void loop(int sockfd, const char* logfile)
+
+
+
+int thread_killer = 0;
+struct ThreadArgs
 {
-    char buff[MAX];
+    int socket;
+    int id;
+    char* buffer;
+};
+
+void thread_cleanup_handler(void* _args)
+{
+    struct ThreadArgs* args = (struct ThreadArgs*)_args;
+    printf("cleaning thread%d..\n",args->id);
+    free(args->buffer);
+    free(args);
+    printf("cleaning done");
+}
+
+   
+void loop(struct ThreadArgs* args, const char* logfile);
+void thread_run(void* threadargs)
+{
+    pthread_cleanup_push(thread_cleanup_handler,threadargs);
+
+    struct ThreadArgs* args = (struct ThreadArgs*)threadargs;
+    char logfile[100] = {'\0'};
+    sprintf(logfile,"log%d.txt",args->id);
+    //loop(args,filename);
+    //    loop(args->socket, filename,args->buffer);
+
+    // Function designed for chat between client and server.
+    //void loop(struct ThreadArgs* args, const char* logfile)
+
+    int sock;
+    sock = args->socket;
+    char* buff = args->buffer;
+
+    printf("debug\n");
     int n;
     // infinite loop for chat
     FILE* fp = fopen(logfile,"a+");
     printf("START READING\n");
-    for (;;) {
+
+    for (;exit_requests[args->id] == 0;) {
         bzero(buff, MAX);
-   
-    printf("READING\n");
         // read the message from client and copy it in buffer
         //int len = recv(sockfd, buff, sizeof(buff),MSG_DONTWAIT);
-        int len = read(sockfd, buff, sizeof(buff));
+        int len = recv(sock, buff, sizeof(buff),0);
+        printf("client%d: ",args->id);
+        for(int i=0;i<len;i++)
+            printf("%c",buff[i]);
+        printf("\n");
+        send(sock,"ok",3,0);
         if(len<=0)
             continue;
         
         // print buffer which contains the client contents
-        printf("From client %d bytes: %s\n",len,buff);
+        printf("From client%d %d bytes: %s\n",args->id,len,buff);
 
         fprintf(fp,"%s\n",buff);
         fflush(fp);
-        //printf("From client: %s\t To client : ", buff);
-        //bzero(buff, MAX);
-        //n = 0;
-        //// copy server message in the buffer
-        //while ((buff[n++] = getchar()) != '\n')
-        //    ;
-   
-        //bzero(buff,sizeof(buff));
-        //strcpy(buff,"accepted");
-        //// and send that buffer to client
-        //write(sockfd, buff, sizeof(buff));
-   
-        // if msg contains "Exit" then server exit and chat ended.
-        //if (strncmp("exit", buff, 4) == 0) {
-        //    printf("Server Exit...\n");
-        //    break;
-        //}
     }
+
+
     fclose(fp);
+    pthread_cleanup_pop(1);
 }
    
+
+volatile short exit_requests[100];
+void end_thread_by_id(int id)
+{
+    exit_requests[id] = 1;
+}
+pthread_t thread_ids[100];
 // Driver function
 int main(int argc, char** argv)
 {
@@ -91,21 +127,44 @@ int main(int argc, char** argv)
     else
         printf("Server listening..\n");
     len = sizeof(cli);
+
+    int client_id = -1;
+    for(int i=0;i<100;i++)
+        exit_requests[i] = 0;
    
-    // Accept the data packet from client and verification
-    connfd = accept(sockfd, (SA*)&cli, &len);
-    if (connfd < 0) {
-        printf("server accept failed...\n");
-        exit(0);
+    while(1)
+    {
+        // Accept the data packet from client and verification
+        connfd = accept(sockfd, (SA*)&cli, &len);
+        if (connfd < 0) {
+            printf("server accept failed...\n");
+            continue;
+            exit(0);
+        }
+        else
+        {
+            client_id++;
+            printf("server accept the client... ID = %d\n",client_id);
+        }
+
+
+        //prepare the log file
+
+        // Function for chatting between client and server
+        //loop(connfd,"log.txt");
+        struct ThreadArgs* thread_args =(struct ThreadArgs*)malloc(sizeof(struct ThreadArgs));
+        thread_args->buffer = (char*)malloc(sizeof(char)*MAX);
+        thread_args->id = client_id;
+        thread_args->socket = connfd;
+        exit_requests[client_id] = 0;
+        int ret = pthread_create(&thread_ids[client_id],NULL,thread_run,thread_args);
+        //printf("debug\n");
+        if(ret!=0)
+        {
+            printf("error pthread_create");
+            exit(1);
+        }
     }
-    else
-        printf("server accept the client...\n");
-   
-    //prepare the log file
-    
-    // Function for chatting between client and server
-    loop(connfd,"log.txt");
-   
     // After chatting close the socket
     close(sockfd);
     // close the file
