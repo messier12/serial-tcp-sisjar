b9eea7604fe33fd5f199ebd18600da86aad2e4e5
Dion <gumball3point@gmail.com>
2021-12-12 19:33:33 +0700

    deleted pthread_kill and pthread_cancel, just for testing

--- tcpserver_main.c
+++ tcpserver_main.c
@@ -112,7 +112,7 @@ void end_thread_by_id(int id)
 {
     exit_requests[id] = 1;
     //if(pthread_kill(thread_ids[id],S)==0)
-        if(pthread_cancel(thread_ids[id])!=0)
+        //if(pthread_cancel(thread_ids[id])!=0)
             printf("ERROR: end thread %d failed\n",id);
 }
 pthread_t thread_ids[100];
